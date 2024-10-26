e4f508cd9c25559891ecf5015fab7a34353cc3e9
LingoJack <3065225677@qq.com>
2024-10-26 14:56:25 +0800

    代码随想录刷题进度推进到两数之和

---
+++ hashtable/hashtable.java
@@ -0,0 +1,191 @@
+package hashtable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class hashtable {
+    
+    /**
+     * 有效的字母异位词
+     * 这是最直接的利用HashMap，但不是最巧妙的
+     * 坏了，忘记java怎么操作str的char了...
+     * Java的语法基础还是不太牢固，先前竟然不知道Map的equals方法已经被重写了
+     */
+    public boolean isAnagram(String s, String t) {
+        int lenS = s.length();
+        int lenT = t.length();
+
+        // 如果长度不同，则直接返回 false
+        if (lenS != lenT) {
+            return false;
+        }
+
+        // 创建两个 HashMap 来存储字符及其出现次数
+        Map<Character, Integer> mapS = new HashMap<>();
+        Map<Character, Integer> mapT = new HashMap<>();
+
+        // 统计字符串 s 中每个字符的出现次数
+        for (int i = 0; i < lenS; i++) {
+            char ch = s.charAt(i);
+            mapS.put(ch, mapS.getOrDefault(ch, 0) + 1);
+        }
+
+        // 统计字符串 t 中每个字符的出现次数
+        for (int i = 0; i < lenT; i++) {
+            char ch = t.charAt(i);
+            mapT.put(ch, mapT.getOrDefault(ch, 0) + 1);
+        }
+
+        // 比较两个 HashMap 是否相等
+        return mapS.equals(mapT);
+    }
+
+    public boolean isAnagramWithArrayASCII(String s, String t) {
+        int[] record = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            // 并不需要记住字符a的ASCII，只要求出一个相对数值就可以了
+            record[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            record[t.charAt(i) - 'a']--;
+        }
+        
+        for (int count: record) {
+            // record数组如果有的元素不为零0，说明字符串s和t 一定是谁多了字符或者谁少了字符。
+            if (count != 0) { 
+                return false;
+            }
+        }
+
+        // record数组所有元素都为零0，说明字符串s和t是字母异位词
+        return true; 
+    }
+
+    /**
+     * 两个数组的交集
+     * 这是最直接，也是最低效率的解法
+     * 这里是直接暴力的两个HashMap统计元素出现的次数
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        for(int i=0;i<len1;i++){
+            int count = map1.getOrDefault(nums1[i], 0);
+            count++;
+            map1.put(nums1[i], count);
+        }
+
+        for(int i=0;i<len2;i++){
+            int count = map2.getOrDefault(nums2[i], 0);
+            count++;
+            map2.put(nums2[i], count);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for(Map.Entry<Integer, Integer> entry : map1.entrySet()){
+            if (entry.getValue() >= 1 && map2.getOrDefault(entry.getKey(), 0) >= 1) {
+                res.add(entry.getKey());
+            }
+        }
+
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+
+    /**
+     * 两个数组的交集
+     * 但是使用Stream API
+     * 基础还是不太牢固，对于Java的集合体系提供的API了解不深，比如HashSet
+     */
+    public int[] intersectionWithStreamAPI(int[] nums1, int[] nums2) {
+        // 使用 IntStream 和 boxed 将数组转换为 HashSet
+        HashSet<Integer> set = IntStream.of(nums2)
+                                        .boxed()
+                                        .collect(Collectors.toCollection(HashSet::new));
+        return Arrays.stream(nums1).boxed().filter(e -> set.contains(e)).distinct().mapToInt(e -> e).toArray();
+    }
+
+    /**
+     * 快乐数
+     * 可恶，初见竟无一点思路...
+     * 快乐数示例：
+     * 1^2 + 9^2 = 82
+     * 8^2 + 2^2 = 68
+     * 6^2 + 8^2 = 100
+     * 1^2 + 0^2 + 0^2 = 1
+     * 最后看答案了，用快慢指针来解决这个，这是人能想出来的吗？？？
+     * 这点的关键是想到：
+     * 如果一个数不是快乐数，那么它的平方和序列一定会循环？？？
+     * 由于是循环的，那么fast绕一圈回来一定会追上show
+     * 如果不是循环的，指 1，那么fast就会原地不动，show会追上fast
+     * 关于“如果一个数不是快乐数，那么它的平方和序列一定会循环”的理由：
+     * > 当你对一个整数进行“每个位上的数字平方和”的操作时，得到的结果是有限的。对于一个 k 位的整数 n，每位数字的最大值为 9，
+     * > 因此每次操作后得到的新数最大不会超过 81k（因为 9^2 = 81）。这意味着无论初始数字多大，经过几次变换之后，结果将被限制在一个相对较小的范围内
+     * > 这个过程生成的数字集合是有限的，并且每一步都是确定性的（给定一个输入，输出是固定的），如果我们不断重复这个过程而没有达到 1，
+     * > 那么根据鸽巢原理（如果有 n 个容器和 n+1 个物品，至少有一个容器包含不止一个物品），最终一定会遇到之前出现过的数字，从而形成一个循环。
+     * > 鸽巢原理的集合论表述：若A是n+1元集，B是n元集，则不存在从A到B的单射。（双射 = 单射 + 满射）
+     */
+    public boolean isHappy(int n) {
+        int fast = n;
+        int slow = n;
+        do {
+            slow=squareSum(slow);
+            fast=squareSum(fast);
+            fast=squareSum(fast);
+            if (fast == 1) {
+                return true;
+            }
+        } while (slow != fast);
+
+        return fast == 1;
+    }
+    
+    private int squareSum(int m){
+        int squaresum=0;
+        while (m != 0) {
+           squaresum += (m % 10) * (m % 10);
+            m /= 10;
+        }
+        return squaresum;
+    }
+
+    /**
+     * 两数之和
+     * 比较早想到了思路，就是实现起来有些细节卡顿了
+     * 我想的这个解法时间复杂度蛮优秀的hhh
+     */
+    public int[] twoSum(int[] nums, int target) {
+        // 这个Map用来存储下标，比如
+        // 2 7 11 15 target=9
+        // 那么遍历到2的时候，会以taget-2=7为key放入2的索引值，遍历每个元素时，先看看该map中以当前值为key有无value，有则取出
+        // map的value加上当前key的索引组成的数组，即为target的两数之和的下标数组
+        Map<Integer, Integer> idxs = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            if (idxs.get(nums[i]) != null) {
+                return new int[]{i, idxs.get(nums[i])};
+            }
+            idxs.put(target - nums[i], i);
+        }
+        return new int[2];
+    }
+
+    /**
+     * 四数相加II
+     */
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        
+        return 0;
+    }
+}
---
+++ linklist/linklist.java
@@ -0,0 +1,298 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class linklist {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 移除链表元素
+     * 涉及 链表 操作中一个非常重要的技巧：虚拟头节点
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode cur = dummyHead;
+        while (cur != null) {
+            if (cur.next != null && cur.next.val == val) {
+                cur.next = cur.next.next;
+            }
+            else {
+                cur = cur.next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+    /**
+     * 反转链表
+     * 这是目前做得最顺利的一题
+     * 不过采用的是新建链表的方式，空间复杂度方面是不如双指针法优秀的
+     */
+    public ListNode reverseList(ListNode head) {
+        
+        // 创建一个新的链表
+        ListNode cur = head;
+        ListNode reverseHead = null;
+
+        // 遍历当前链表，把每个节点都插入到新链表的头部
+        while (cur != null) {
+            ListNode node = new ListNode(cur.val);
+            // 往头部插入
+            node.next = reverseHead;
+            reverseHead = node;
+            cur = cur.next;
+        }
+
+        return reverseHead;
+    }
+
+    /**
+     * 反转链表
+     * 双指针实现
+     * 还有就是清楚java的拷贝默认是值拷贝这一点
+     * 将一个对象引用赋值给另一个变量时，实际上是在进行地址的值拷贝，也就是浅拷贝
+     */
+    public ListNode reverseListWithDoublePointer(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode temp = null;
+        while (cur != null) {
+            
+            // 因为后面要改变cur的next指针的指向，所以先要用temp保存一下
+            temp = cur.next;
+
+            // 反转指向
+            cur.next = prev;
+
+            // 更新左指针
+            prev = cur;
+
+            // 更新右指针
+            cur = temp;
+        }
+        return prev;
+    }
+
+    /**
+     * 反转链表
+     * 递归实现
+     */
+    public ListNode reverseListWithRecursion(ListNode head) {
+        return reverse(null, head);
+    }
+
+    public ListNode reverse(ListNode prev, ListNode cur) {
+
+        if (cur == null) {
+            return prev;
+        }
+
+        // 暂存
+        ListNode tmp = null;
+        
+        tmp = cur.next;
+
+        // 反转
+        cur.next = prev;
+
+        return reverse(cur, tmp);
+    }
+
+    /**
+     * 两两交换链表中的节点
+     * 解决的技巧依旧是虚拟头节点
+     * 但是重点是要画图而且理清操作的先后顺序
+     * 还有就是清楚java的拷贝默认是值拷贝这一点
+     * 将一个对象引用赋值给另一个变量时，实际上是在进行地址的值拷贝，也就是浅拷贝
+     */
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+
+        ListNode firstNode = null;
+        ListNode secondNode = null;
+        
+        ListNode cur = dummyHead;
+        while (cur.next != null && cur.next.next != null) {
+            
+            // 暂存两个节点
+            firstNode = cur.next;
+
+            // 这里的secondNode是 1 -> 2(first) -> 3 -> 4(second)
+            secondNode = cur.next.next.next;
+
+            // 步骤一
+            cur.next = firstNode.next;
+
+            // 步骤二
+            firstNode.next.next = firstNode;
+
+            // 步骤三
+            firstNode.next = secondNode;
+            cur = cur.next.next;
+        }
+
+        // 不满足两两配对的情况，不交换
+        return dummyHead.next;
+    }
+
+    /**
+     * 删除链表的倒数第N个节点
+     * 这里我是用双指针来标记需要标记删除的倒数第n个节点
+     * 链表的一大问题就是操作当前节点必须要找前一个节点才能操作。这就造成了，头结点的尴尬，因为头结点没有前一个节点了。
+     * 每次对应头结点的情况都要单独处理，所以使用虚拟头结点的技巧，就可以解决这个问题。
+     * 用到的技巧就是虚拟头节点，这样方便处理删除实际头结点的逻辑
+     * 需要特殊考虑的就是，当被删除的节点是头节点的时候，需要设置新的头节点
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+
+        // 目前看可以考虑使用双指针
+        ListNode show = dummyHead;
+        ListNode fast = dummyHead;
+
+        int countFast = 0;
+        int countShow = 0;
+
+        // n = 1
+        // d 1 
+        // s
+        //   f
+        //   h
+        
+        // show应该比fast滞后n个位置
+        while (fast.next != null) {
+            // fast遍历链表，保持show在fast后n个位置
+            if (countFast - countShow >= n) {
+                show = show.next;
+                countShow++;
+            }
+            countFast++;
+            fast = fast.next;
+        }
+
+        // fast遍历到尾部，移除show即可
+        show.next = show.next.next;
+
+        // 处理被移除的是头节点情况
+        if (show == dummyHead) {
+            head = head.next;
+        }
+    
+        return head;
+    }
+
+    /**
+     * 链表相交
+     * 本题的难度主要在题目描述不说人话
+     * 需要注意的点是相等不止是值相等，next指针也得相等
+     * 求出两个链表长度的差值，然后尾部对齐，比较cur，不等于则同时往后移动cur
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = 0;
+        int lenB = 0;
+
+        ListNode curA = headA;
+        ListNode curB = headB;
+
+        // 确定A表长度
+        while (curA != null) {
+            lenA++;
+            curA = curA.next;
+        }
+
+        // 确定B表长度
+        while (curB != null) {
+            lenB++;
+            curB = curB.next;
+        }
+
+        ListNode shortOne = null;
+        ListNode longOne = null;
+
+        // 比较长度得到较短的表
+        if (lenA > lenB) {
+            shortOne = headB;
+            longOne = headA;
+        }
+        else {
+            shortOne = headA;
+            longOne = headB;
+        }
+        
+        // 4 1 8 4 5 len=5
+        // 5 0 1 8 4 5 len=6
+        // 移动使两链表尾部对齐
+        int diff = (lenA - lenB) < 0 ? (lenB - lenA) : (lenA - lenB);
+        for(int i=0;i<diff;i++){
+            longOne = longOne.next;
+        }
+
+        // 比较，若相等则直接返回，否则两链表的cur指针同时右移，直到为null
+        while (shortOne != null && longOne != null) {
+            if (shortOne == longOne) {
+                return shortOne;
+            }
+            shortOne = shortOne.next;
+            longOne = longOne.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * 环形链表 II
+     * 感觉又是题目描述的问题
+     * 首先想到的是 HashMap 暴力解
+     * 虽然是一下就通过了，但是执行用时并不优秀
+     */
+    public ListNode detectCycle(ListNode head) {
+        
+        ListNode cur = head;
+
+        Map<ListNode, Integer> map = new HashMap<>();
+        
+        // 如果是环形链表，这里就会一直循环
+        while (cur != null) {
+            Integer count = map.get(cur);
+            count = count == null ? 0 : count;
+            if (count == 1) {
+                return cur;
+            }
+            count++;
+            map.put(cur, count);
+            cur = cur.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * 环形链表 II
+     * 快慢指针解法
+     * 这个思路不容易想到，而且涉及一些数学知识和相对运动观点
+     */
+    public ListNode detectCycleWithShowFastPointer(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {// 有环
+                ListNode index1 = fast;
+                ListNode index2 = head;
+                // 两个指针，从头结点和相遇结点，各走一步，直到相遇，相遇点即为环入口
+                while (index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+        return null;
+    }
+}
+
