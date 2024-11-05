785b4705d731eabdc810880e1fddb40f020ac5d5
LingoJack <3065225677@qq.com>
2024-11-05 22:16:23 +0800

    进度推进到双指针模块的移除元素的题目（已做完该题）

--- string/string.java
+++ string/string.java
@@ -167,27 +167,97 @@ public class string {
 
     /**
      * 实现 strStr()
+     * 这个涉及到KMP算法，关键是要求前缀表
+     * 你要弄清楚前缀表的概念，并且要掌握求前缀表的逻辑
+     * 这道题不是我自己写出来的
      */
     public int strStr(String haystack, String needle) {
-       if (needle.length() == 0) {
+        if (needle.isEmpty()) {
             return 0;
-       }
-
-       // 构建前缀表
-       int[] prefix = buildPrefixTable(needle);
-
-        // 
-        char[] pattern = needle.toCharArray();
-        char[] str = haystack.toCharArray();
-
-        int lt = 0;
-        int rt = 0;
-
-        while () {
-            
         }
+        if (haystack.isEmpty() || haystack.length() < needle.length()) {
+            return -1;
+        }
+    
+        int[] prefix = buildPrefixTable(needle);
+        // j是指向待匹配的模板的，即needle
+        int j = 0;
 
-
+        // aabaaabaaac
+        //       i
+        // aabaaac
+        //   j
+        // aabaaac
+        // 0101220
+    
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = prefix[j - 1]; // 使用前缀表跳过重复匹配
+            }
+    
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+    
+            if (j == needle.length()) {
+                return i - j + 1; // 找到匹配位置
+            }
+        }
+    
+        return -1;
     }
 
+    /**
+     * KMP前缀表的构建是一个难点
+     * 一个串中查找是否出现过另一个串，这是KMP的看家本领
+     * todo 需要理解构建的逻辑
+     */
+    private int[] buildPrefixTable(String s) {
+        char[] str = s.toCharArray();
+        int[] prefix = new int[str.length];
+        int j = 0;
+        prefix[0] = 0;
+
+        // a b e a b f
+        //     j
+        //           i
+        // 0 0 0 1 2 0
+
+        // 计算字符串每一位的最长前后缀
+        for (int i = 1; i < str.length; i++) {
+            while (j > 0 && str[j] != str[i]) {
+                // 回退到上一次匹配的前缀的前一个位置
+                j = prefix[j - 1];
+            }
+
+            // str[i] == str[j] or j == 0
+            if (str[j] == str[i]) {
+                j++;
+            }
+            prefix[i] = j;
+        }
+        return prefix;
+    }
+
+    /**
+     * 重复的子字符串
+     * 这个感觉有点像构建KMP的前缀表的逻辑
+     * 字符串应该是我的弱点
+     * 这个是大神的顶级妙解
+     * 思路解析如下：
+     * 假设判断字符串s（n个字符构成）是不是由重复子串构成的，我们先将两个字串拼成一个大的字符串：S = s + s（S 有2n个字符）；
+     * 字符串拼接的最底层思想应该是使用字符串移位来判断字符串是否由重复子串（循环节）构成，
+     * 因为S这个大的字符串 其实包含了字符串s的所有移位字符串。令字符串字符索引从0开始，[m,n]表示S中索引为m到索引为n的这一段字符，索引为闭区间，
+     * 则[0,n-1]即S的前一半字符表示原始字符串s；[1,n]表示s右移1位的状态；[2,n+1]表示s右移2位的状态；······；[n,2n-1]即S的后半拉字符串可以理解为s移n位的状态，此时s已经移回了原始的状态了；
+     * 此外我们不难知道如果一个没有循环节的字符串在移位时必须要右移n次他才能移回它的原始状态，而有循环节的字符串最多只需要n/2次（只有两个循环节）；
+     * 所以当S砍去首尾字符时，对于没有循环节的字符串s，相当于砍去了[0,n-1]的原始状态和[n,2n-1]这个移位n次的又回归原始的状态，
+     * 我们在[1,2n-2]范围内只能找到s移位1，2，···，n-1位时的状态，
+     * 所以在[1,2n-2]内是不存在无循环节的s的；但是对于有循环节的s来说，n/2 <= n-1，所以一定存在至少一个移位状态为s，
+     * 即最少存在一个s（其实对于有循环节的s来说，不考虑移位状态我们也能明白[1,2n-2]
+     * 内一定至少有一个s，例如对于有两个循环节的s：组成S的前一个s的后一半字符 + 组成S的后一个s的前一半的字符 == s；三个循环节的s更不用说了)；
+     */
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s + s;
+        return str.substring(1, str.length() - 1).contains(s);
+    }
 }
