14e5b343056e8138a6144f79d6b395725c902386
1-6 <a1-6@1-6s-MacBook-Air.local>
2024-12-07 19:20:50 +0800

    推进到回溯模块的组合问题

--- backtracking/backtracking.java
+++ backtracking/backtracking.java
@@ -1,5 +1,21 @@
 package backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class backtracking {
-    
+
+    private List<List<Integer>> combinationList = new ArrayList<>();
+    private List<Integer> conbinationPath = new ArrayList<>();
+    /**s
+     * 组合
+     * [1, n]中k个数的组合
+     */
+    public List<List<Integer>> combine(int n, int k) {
+        
+    }
+
+    private void combineBacktracking(List<Integer> list, int n, int k) {
+        
+    }
 }
