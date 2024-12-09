d49aa7da5c6e99c5701ca1068e2962f6ac9e26a2
LingoJack <3065225677@qq.com>
2024-12-09 23:45:58 +0800

    推进到组合总和3

--- backtracking/backtracking.java
+++ backtracking/backtracking.java
@@ -1,21 +1,54 @@
 package backtracking;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class backtracking {
 
-    private List<List<Integer>> combinationList = new ArrayList<>();
-    private List<Integer> conbinationPath = new ArrayList<>();
-    /**s
+    List<List<Integer>> result4Combina = new ArrayList<>();
+    LinkedList<Integer> path4Combina = new LinkedList<>();
+
+    /**
      * 组合
+     * 这是一道典型的回溯，回溯题是有一定的模板可以帮助思考的
      * [1, n]中k个数的组合
+     * 组合不管顺序，所以要从前往后取，每次取都不往前
+     * 而且进行剪枝
      */
     public List<List<Integer>> combine(int n, int k) {
+        backtracking4Combine(n, k, 1);
+        return result4Combina;
+    }
+
+    private void backtracking4Combine(int n, int k, int startIndex) {
+        // 终止条件
+        if (path4Combina.size() == k) {
+            // 存放结果
+            result4Combina.add(new ArrayList<>(path4Combina));
+            // 返回
+            return;
+        }
+
+        // 本层要处理的集合中的元素
+        for (int i = startIndex; i <= n - (k - path4Combina.size()) + 1; i++) {
+            // 处理节点
+            path4Combina.add(i);
+            // 递归
+            backtracking4Combine(n, k, i + 1);
+            // 回溯，撤销处理结果
+            path4Combina.removeLast();
+        }
+    }
+    
+    /**
+     * 组合总和III
+     */
+    public List<List<Integer>> combinationSum3(int k, int n) {
         
     }
 
-    private void combineBacktracking(List<Integer> list, int n, int k) {
+    private void backtracking4CombinationSum(List<List<Integer>> res, List<Integer> list, ) {
         
     }
 }
