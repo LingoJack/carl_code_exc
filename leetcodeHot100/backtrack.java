104cb992bfaab28b2322725f3aa63b6492e1d8b1
lingojack <3065225677@qq.com>
2025-01-18 18:38:14 +0800

    最小覆盖子串

--- leetcodeHot100/backtrack.java
+++ leetcodeHot100/backtrack.java
@@ -2,7 +2,9 @@ package leetcodeHot100;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class backtrack {
     /**
@@ -61,7 +63,7 @@ public class backtrack {
             return true;
         }
         visited[i][j] = true;
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int[][] directions = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
         boolean result = false;
         for (int[] dir : directions) {
             int newi = i + dir[0], newj = j + dir[1];
@@ -101,7 +103,7 @@ public class backtrack {
             res.add(new ArrayList<>(list));
             return;
         }
-        for(int i = start; i < s.length(); i++) {
+        for (int i = start; i < s.length(); i++) {
             String subString = s.substring(start, i + 1);
             if (isPalindromeString[start][i] || isPalindromeString(subString)) {
                 isPalindromeString[start][i] = true;
@@ -121,6 +123,58 @@ public class backtrack {
             }
             rt--;
             lt++;
+        }
+        return true;
+    }
+
+    /**
+     * 最小覆盖子串
+     * 没做出来
+     */
+    public String minWindow(String s, String t) {
+        Map<Character, Integer> tCharCount = new HashMap<>();
+        for (char ch : t.toCharArray()) {
+            tCharCount.put(ch, tCharCount.getOrDefault(ch, 0) + 1);
+        }
+
+        Map<Character, Integer> windowCounts = new HashMap<>();
+        int required = tCharCount.size();
+        int formed = 0;
+        int l = 0, r = 0;
+        int start = -1;
+        int minLen = Integer.MAX_VALUE;
+
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
+
+            if (tCharCount.containsKey(c) && windowCounts.get(c).equals(tCharCount.get(c))) {
+                formed++;
+            }
+
+            while (l <= r && formed == required) {
+                c = s.charAt(l);
+                if (r - l + 1 < minLen) {
+                    start = l;
+                    minLen = r - l + 1;
+                }
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (tCharCount.containsKey(c) && windowCounts.get(c) < tCharCount.get(c)) {
+                    formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+
+        return start == -1 ? "" : s.substring(start, start + minLen);
+    }
+
+    private boolean valid(Map<Character, Integer> tMap, Map<Character, Integer> windowMap) {
+        for (Character ch : tMap.keySet()) {
+            if (windowMap.getOrDefault(ch, 0) < tMap.get(ch)) {
+                return false;
+            }
         }
         return true;
     }
