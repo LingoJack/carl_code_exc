b9270c6d67737ce1fb31fc34b337ed8a69556cb4
lingojack <3065225677@qq.com>
2025-01-21 11:08:14 +0800

    1

--- leetcodeHot100/matrix.java
+++ leetcodeHot100/matrix.java
@@ -1,6 +1,8 @@
 package leetcodeHot100;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class matrix {
@@ -69,4 +71,85 @@ public class matrix {
         }
         return res;
     }
+
+    /**
+     * 矩阵置零
+     * 7min ac 击败51%
+     * 但是空间复杂度太高，不符合题目要求
+     */
+    public void setZeroes(int[][] matrix) {
+        Deque<int[]> queue = new ArrayDeque<>();
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                if(matrix[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+        while(!queue.isEmpty()) {
+            int[] index = queue.poll();
+            for(int i = 0; i < row; i++) {
+                matrix[i][index[1]] = 0;
+            }
+            for(int i = 0; i < col; i++) {
+                matrix[index[0]][i] = 0;
+            }
+        }
+    }
+
+    /**
+     * 矩阵置零
+     * 优化空间复杂度
+     */
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        boolean row0ContainZero = false;
+        boolean col0ContainZero = false;
+        boolean[] rowConatinZero = new boolean[row];
+        boolean[] colContainZero = new boolean[col];
+        for(int i = 0; i < col; i++) {
+            if(matrix[0][i] == 0) {
+                row0ContainZero = true;
+                break;
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                col0ContainZero = true;
+                break;
+            }
+        }
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                if(matrix[i][j] == 0) {
+                    rowConatinZero[i] = true;
+                    colContainZero[j] = true;
+                }
+            }
+        }
+        if(row0ContainZero) {
+            for (int i = 0; i < col; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if(col0ContainZero) {
+            for(int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (rowConatinZero[i] || colContainZero[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    /**
+     * 旋转图像
+     */
 }
