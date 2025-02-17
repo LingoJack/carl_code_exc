fcfe08981756d4d18a9e61879925cfd3a25eea9c
LingoJack <3065225677@qq.com>
2025-02-17 14:47:01 +0800

    1

--- leetcodeHot100/LeetcodeHot00TwoEx.java
+++ leetcodeHot100/LeetcodeHot00TwoEx.java
@@ -143,49 +143,193 @@ public class LeetcodeHot00TwoEx {
      * 排序链表
      */
     public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // 3 1 2 5 4
+        ListNode[] spilt = spilt(head);
+        ListNode ltPart = sortList(spilt[0]);
+        ListNode rtPart = sortList(spilt[1]);
+        ListNode dummy = new ListNode();
+        ListNode last = dummy;
+        while (ltPart != null && rtPart != null) {
+            if (ltPart.val < rtPart.val) {
+                last.next = ltPart;
+                last = ltPart;
+                ltPart = ltPart.next;
+            } else {
+                last.next = rtPart;
+                last = rtPart;
+                rtPart = rtPart.next;
+            }
+        }
+        last.next = rtPart == null ? ltPart : rtPart;
+        return dummy.next;
+    }
 
+    private ListNode[] spilt(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        // 3 1 2 5 4
+        // s
+        // f
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode next = slow.next;
+        slow.next = null;
+        return new ListNode[] { head, next };
     }
 
     /**
      * 二叉树展开为链表
      */
     public void flatten(TreeNode root) {
-
+        TreeNode node = root;
+        while (node != null) {
+            if (node.left != null) {
+                TreeNode rt = node.right;
+                node.right = node.left;
+                node.left = null;
+                TreeNode findRight = node.right;
+                while (findRight != null && findRight.right != null) {
+                    findRight = findRight.right;
+                }
+                findRight.right = rt;
+            }
+            node = node.right;
+        }
     }
 
     /**
      * 从前序遍历和中序遍历序列构造二叉树
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // 递归得构造左右子树，限定构造的范围
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return getRoot(preorder, inorder, map, 0, inorder.length - 1);
+    }
 
+    private int preorderIndex = 0;
+
+    private TreeNode getRoot(int[] preorder, int[] inorder, Map<Integer, Integer> map, int start, int end) {
+        if (end < start) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preorderIndex++]);
+        int inorderIndex = map.get(root.val);
+        root.left = getRoot(preorder, inorder, map, start, inorderIndex - 1);
+        root.right = getRoot(preorder, inorder, map, inorderIndex + 1, end);
+        return root;
     }
 
     /**
      * 组合总和
+     * 没做出来
+     * 你没有理解做回溯题的方法论，要画树状图去理解做
+     * 不要那么想当然，你可以去看你以前的解法，这个dfs内部的for循环是可以不要的
      */
-    public List<List<Integer>> combinationSumTwoEx(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(candidates, res, new ArrayList<>(), target, 0);
+        return res;
+    }
 
+    private void dfs(int[] candidates, List<List<Integer>> res, List<Integer> list, int target, int start) {
+        if (target == 0) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        if (start == candidates.length) {
+            return;
+        }
+        dfs(candidates, res, list, target, start + 1);
+        if (target >= candidates[start]) {
+            list.add(candidates[start]);
+            dfs(candidates, res, list, target - candidates[start], start);
+            list.remove(list.size() - 1);
+        }
     }
 
     /**
      * 分割回文串
      */
     public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        dfs(s, res, new ArrayList<>(), 0);
+        return res;
+    }
 
+    private void dfs(String s, List<List<String>> res, List<String> list, int start) {
+        if (start == s.length()) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = start; i < s.length(); i++) {
+            if (valid(s, start, i)) {
+                list.add(s.substring(start, i + 1));
+                dfs(s, res, list, i + 1);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    private boolean valid(String s, int start, int end) {
+        int lt = start;
+        int rt = end;
+        while (rt > lt) {
+            if (s.charAt(rt) != s.charAt(lt)) {
+                return false;
+            }
+            rt--;
+            lt++;
+        }
+        return true;
     }
 
     /**
      * 最长递增子序列
+     * 要求不准用dp，而用O（n）时间复杂度的算法
      */
     public int lengthOfLIS(int[] nums) {
+        // 3 1 2 5 4
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (list.isEmpty() || list.get(list.size() - 1) < num) {
+                list.add(num);
+            } else {
+                list.set(binarySearch(list, num), num);
+            }
+        }
+        return list.size();
+    }
 
+    private int binarySearch(List<Integer> list, int target) {
+        // 1 2 5
+        int lt = 0;
+        int rt = list.size() - 1;
+        while (rt >= lt) {
+            int mid = (lt + rt) / 2;
+            if (list.get(mid) > target) {
+                rt = mid - 1;
+            } else if (list.get(mid) < target) {
+                lt = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return lt;
     }
 
     /**
      * 复原ip地址
      */
     public List<String> restoreIpAddresses(String s) {
-
+        
     }
 
     /**
