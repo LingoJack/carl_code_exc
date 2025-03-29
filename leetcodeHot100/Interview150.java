43813d71be694d4f7a32024b4691e164e89c1eb1
lingojack <3065225677@qq.com>
2025-03-29 14:43:39 +0800

    1

--- leetcodeHot100/Interview150.java
+++ leetcodeHot100/Interview150.java
@@ -1934,4 +1934,101 @@ public class Interview150 {
         }
         return money;
     }
+
+    /**
+     * 被围绕的区域
+     * 这个解法虽然能做，但是不是最优的思路
+     * 可以考虑从边缘出发
+     */
+    public void solve(char[][] board) {
+        int row = board.length;
+        int col = board[0].length;
+        boolean[][] visited = new boolean[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == 'X' || visited[i][j]) {
+                    continue;
+                }
+                List<int[]> list = new ArrayList<>();
+                if (dfs(board, list, i, j, visited)) {
+                    for (int[] idx : list) {
+                        board[idx[0]][idx[1]] = 'X';
+                    }
+                }
+            }
+        }
+        // ["O","O","O","O","X","X"],
+        // ["O","O","O","O","O","O"],
+        // ["O","X","O","X","O","O"],
+        // ["O","X","O","O","X","O"],
+        // ["O","X","O","X","O","O"],
+        // ["O","X","O","O","O","O"]
+    }
+
+    private boolean dfs(char[][] board, List<int[]> list, int i, int j, boolean[][] visited) {
+        int row = board.length;
+        int col = board[0].length;
+        if (!(i >= 0 && i < row && j >= 0 && j < col)) {
+            return false;
+        }
+        if (board[i][j] == 'X' || visited[i][j]) {
+            return true;
+        }
+        list.add(new int[] { i, j });
+        visited[i][j] = true;
+        boolean down = dfs(board, list, i + 1, j, visited);
+        boolean up = dfs(board, list, i - 1, j, visited);
+        boolean left = dfs(board, list, i, j - 1, visited);
+        boolean right = dfs(board, list, i, j + 1, visited);
+        return down && up && left && right;
+    }
+
+    public void solveBetterSolution(char[][] board) {
+        int row = board.length;
+        int col = board[0].length;
+        for (int i = 0; i < row; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, col - 1);
+        }
+        for (int i = 0; i < col; i++) {
+            dfs(board, 0, i);
+            dfs(board, row - 1, i);
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        // ["O","O","O","O","X","X"],
+        // ["O","O","O","O","O","O"],
+        // ["O","X","O","X","O","O"],
+        // ["O","X","O","O","X","O"],
+        // ["O","X","O","X","O","O"],
+        // ["O","X","O","O","O","O"]
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        int row = board.length;
+        int col = board[0].length;
+        if (!(i >= 0 && i < row && j >= 0 && j < col)) {
+            return;
+        }
+        if (board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = 'A';
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+
+    /**
+     * 克隆图
+     */
+    
 }
