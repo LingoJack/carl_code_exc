57694e8fa28895235264979f30c2d07fc47c22fa
lingojack <3065225677@qq.com>
2025-04-07 10:41:50 +0800

    1

--- leetcodeHot100/Interview150.java
+++ leetcodeHot100/Interview150.java
@@ -4013,11 +4013,64 @@ public class Interview150 {
 
     /**
      * 最大正方形
-     * TODO：
+     * 暴力解法居然也过了，击败5.5%
      */
     public int maximalSquare(char[][] matrix) {
-        int res = 0;
+        int maxLen = 0;
+        int row = matrix.length, col = matrix[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] != '1') {
+                    continue;
+                }
+                while (isSquare(matrix, i, j, maxLen + 1)) {
+                    maxLen++;
+                }
+            }
+        }
+        return maxLen * maxLen;
+    }
 
-        return res;
+    private boolean isSquare(char[][] matrix, int rowIdx, int colIdx, int len) {
+        int row = matrix.length, col = matrix[0].length;
+        for (int i = rowIdx; i < rowIdx + len; i++) {
+            for (int j = colIdx; j < colIdx + len; j++) {
+                if (!check(row, col, i, j) || matrix[i][j] != '1') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean check(int row, int col, int rowIdx, int colIdx) {
+        return (rowIdx >= 0 && rowIdx < row) && (colIdx >= 0 && colIdx < col);
+    }
+
+    /**
+     * 最大正方形
+     * DP解法
+     */
+    public int maximalSquareWithDp(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int row = matrix.length, col = matrix[0].length;
+        // dp(i,j) 表示以 (i,j) 为右下角，且只包含 1 的正方形的边长最大值
+        int[][] dp = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        return maxSide * maxSide;
     }
 }
