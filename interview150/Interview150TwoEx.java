c05b2b37c3423c3dd84d0c51885c831b706ebca9
lingojack <3065225677@qq.com>
2025-04-08 13:46:52 +0800

    1

---
+++ interview150/Interview150TwoEx.java
@@ -0,0 +1,49 @@
+package interview150;
+
+public class Interview150TwoEx {
+
+    /**
+     * 合并两个有序数组
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx1 = m - 1;
+        int idx2 = n - 1;
+        int idx = m + n - 1;
+        while (idx1 >= 0 || idx2 >= 0) {
+            int num1 = idx1 >= 0 ? nums1[idx1] : Integer.MIN_VALUE;
+            int num2 = idx2 >= 0 ? nums2[idx2] : Integer.MIN_VALUE;
+            if (num1 > num2) {
+                idx1--;
+                nums1[idx] = num1;
+            } else {
+                idx2--;
+                nums1[idx] = num2;
+            }
+            idx--;
+        }
+    }
+
+    /**
+     * 移除元素
+     */
+    public int removeElement(int[] nums, int val) {
+        int fast = 0, slow = 0;
+        while (fast < nums.length) {
+            if(nums[fast] != val) {
+                swap(nums, fast, slow++);
+            }
+            fast++;
+        }
+        return slow;
+    }
+
+    private void swap(int[] nums, int idx1, int idx2) {
+        int t = nums[idx1];
+        nums[idx1] = nums[idx2];
+        nums[idx2] = t;
+    }
+
+    /**
+     * 
+     */
+}
