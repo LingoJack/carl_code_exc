de43f11b5c3e63cdbc10347fe53a4875fbac4f16
lingojack <3065225677@qq.com>
2025-04-09 19:49:56 +0800

    1

--- interview150/Interview150TwoEx.java
+++ interview150/Interview150TwoEx.java
@@ -64,4 +64,28 @@ public class Interview150TwoEx {
         }
         return winner;
     }
+
+    /**
+     * 轮转数组
+     */
+    public void rotate(int[] nums, int k) {
+        // 1 2 3 4 5 6 7 k=3
+        // 5 6 7 1 2 3 4
+        int len = nums.length;
+        k %= len;
+        reverse(nums, 0, len - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, len - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        if (end <= start) {
+            return;
+        }
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
 }
