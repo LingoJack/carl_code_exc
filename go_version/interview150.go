005333871e224ff8c7d6137633de40ef7602e27a
lingojack <3065225677@qq.com>
2025-04-12 18:24:32 +0800

    1

--- go_version/interview150.go
+++ go_version/interview150.go
@@ -4,6 +4,7 @@ import (
 	"container/list"
 	"fmt"
 	"math"
+	"sort"
 	"strings"
 )
 
@@ -291,4 +292,231 @@ func reverse(s string) string {
 		rt--
 	}
 	return s
+}
+
+// 找出字符串中第一个匹配项的下标
+func strStr(haystack string, needle string) int {
+	i, match := 0, 0
+	partialMatchTable := preprocess(needle)
+	for i < len(haystack) {
+		if haystack[i] == needle[match] {
+			match++
+			if match == len(needle) {
+				return i - len(needle) + 1
+			}
+			i++
+			continue
+		}
+		if match > 0 {
+			match = partialMatchTable[match-1]
+		} else {
+			i++
+		}
+	}
+	return -1
+}
+
+func preprocess(needle string) []int {
+	l := len(needle)
+	table := make([]int, l)
+	idx, matchIdx := 1, 0
+	for idx < len(needle) {
+		if needle[idx] == needle[matchIdx] {
+			matchIdx++
+			table[idx] = matchIdx
+			idx++
+			continue
+		}
+		if matchIdx > 0 {
+			matchIdx = table[matchIdx-1]
+		} else {
+			table[idx] = 0
+			idx++
+		}
+	}
+	return table
+}
+
+// 盛最多水的容器
+func maxArea(height []int) int {
+	lt, rt := 0, len(height)-1
+	res := 0
+	for lt < rt {
+		w := rt - lt
+		h := min(height[lt], height[rt])
+		res = max(res, w*h)
+		if height[lt] < height[rt] {
+			lt++
+		} else {
+			rt--
+		}
+	}
+	return res
+}
+
+// 三数之和
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	res := make([][]int, 0)
+	first := 0
+	for first < len(nums)-2 {
+		second, third := first+1, len(nums)-1
+		for second < third {
+			sum := nums[first] + nums[second] + nums[third]
+			if sum == 0 {
+				res = append(res, []int{nums[first], nums[second], nums[third]})
+				second++
+				for second < third && nums[second] == nums[second-1] {
+					second++
+				}
+				third--
+				for second < third && nums[third] == nums[third+1] {
+					third--
+				}
+			} else if sum < 0 {
+				second++
+			} else {
+				third--
+			}
+		}
+		first++
+		for first > 0 && first < len(nums)-2 && nums[first] == nums[first-1] {
+			first++
+		}
+	}
+	return res
+}
+
+// 长度最小的子数组
+func minSubArrayLen(target int, nums []int) int {
+	lt, rt, sum, res, hasFound := 0, 0, 0, len(nums), false
+	for rt < len(nums) {
+		sum += nums[rt]
+		for sum >= target {
+			hasFound = true
+			res = min(res, rt-lt+1)
+			sum -= nums[lt]
+			lt++
+		}
+		rt++
+	}
+	if !hasFound {
+		return 0
+	}
+	return res
+}
+
+// 无重复字符的最长子串
+func lengthOfLongestSubstring(s string) int {
+	charMap := make(map[rune]bool)
+	runes := []rune(s)
+	lt, rt, len, max := 0, 0, len(runes), 0
+	for rt < len {
+		for charMap[runes[rt]] {
+			delete(charMap, runes[lt])
+			lt++
+		}
+		charMap[runes[rt]] = true
+		if max < rt-lt+1 {
+			max = rt - lt + 1
+		}
+		rt++
+	}
+	return max
+}
+
+// 最小覆盖子串
+func minWindow(s string, t string) string {
+	need, have := make(map[rune]int), make(map[rune]int)
+	sRunes, tRunes := []rune(s), []rune(t)
+	for _, r := range tRunes {
+		need[r]++
+	}
+	lt, rt := 0, 0
+	valid := 0
+	start, minLen := -1, math.MaxInt
+	for rt < len(sRunes) {
+		rc := sRunes[rt]
+		if _, exist := need[rc]; exist {
+			have[rc]++
+			if have[rc] == need[rc] {
+				valid++
+			}
+		}
+		for valid >= len(need) {
+			if minLen > rt-lt+1 {
+				minLen = rt - lt + 1
+				start = lt
+			}
+			lc := sRunes[lt]
+			if _, exist := need[lc]; exist {
+				if have[lc] == need[lc] {
+					valid--
+				}
+				have[lc]--
+			}
+			lt++
+		}
+		rt++
+	}
+	if start == -1 {
+		return ""
+	}
+	return s[start : start+minLen]
+}
+
+// 螺旋矩阵
+func spiralOrder(matrix [][]int) []int {
+	row, col := len(matrix), len(matrix[0])
+	res := make([]int, 0)
+	rowIdx, colIdx := 0, 0
+	upBound, downBound, leftBound, rightBound := 0, row-1, 0, col-1
+	count, total := 0, row*col
+	for count < total {
+		for i := leftBound; count < total && i <= rightBound; i++ {
+			res = append(res, matrix[rowIdx][i])
+			colIdx = i
+			count++
+		}
+		upBound++
+		for i := upBound; count < total && i <= downBound; i++ {
+			res = append(res, matrix[i][colIdx])
+			rowIdx = i
+			count++
+		}
+		rightBound--
+		for i := rightBound; count < total && i >= leftBound; i-- {
+			res = append(res, matrix[rowIdx][i])
+			colIdx = i
+			count++
+		}
+		downBound--
+		for i := downBound; count < total && i >= upBound; i-- {
+			res = append(res, matrix[i][colIdx])
+			rowIdx = i
+			count++
+		}
+		leftBound++
+	}
+	return res
+}
+
+// 旋转图像
+func rotate(nums [][]int) {
+	row, col := len(nums), len(nums[0])
+	for i := 0; 2*i < col; i++ {
+		for j := 0; j < col; j++ {
+			nums[i][j], nums[row-1-i][j] = nums[row-1-i][j], nums[i][j]
+		}
+	}
+	for i := 0; i < row; i++ {
+		for j := 0; j < i; j++ {
+			nums[i][j], nums[j][i] = nums[j][i], nums[i][j]
+		}
+	}
+}
+
+// 矩阵置零
+func setZeroes(matrix [][]int) {
+
 }
--- interview150/Interview150TwoEx.java
+++ interview150/Interview150TwoEx.java
@@ -2,7 +2,11 @@ package interview150;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Interview150TwoEx {
 
@@ -185,53 +189,65 @@ public class Interview150TwoEx {
      * 找出字符串中第一个匹配项的下标
      * 没做出来
      * 这个果然涉及KMP算法（其实也可以不涉及KMP，滑动窗口就可以）
-     * 就是针对needle的[0, i]建立前缀表
-     * 比如
-     * a a b a d a a c a a e a c
-     * a a e a c
-     * 1 2 0 1 0
      */
-    public int strStrError(String haystack, String needle) {
-        int[] prefixTable = buildPrefixTable(needle);
-        for (int i : prefixTable) {
-            System.out.print(i + " ");
+    public int strStr(String haystack, String needle) {
+        int hLen = haystack.length(), nLen = needle.length();
+        if (nLen == 0) {
+            return 0;
         }
-        int idx = 0;
-        int matchIdx = 0;
-        int nLen = needle.length();
-        int hLen = haystack.length();
+        // 构建真前后缀表
+        int[] partialMathchTable = preprocess(needle);
+        // 根据前后缀表执行相应的逻辑
+        int idx = 0, matchIdx = 0;
         while (idx < hLen) {
+            while (matchIdx > 0 && haystack.charAt(idx) != needle.charAt(matchIdx)) {
+                matchIdx = partialMathchTable[matchIdx - 1];
+            }
             if (haystack.charAt(idx) == needle.charAt(matchIdx)) {
                 matchIdx++;
-                idx++;
-                if (matchIdx == nLen) {
-                    return idx - nLen + 1;
-                }
-                continue;
             }
-            idx += prefixTable[matchIdx];
-            matchIdx = 0;
+            if (matchIdx == nLen) {
+                return idx - nLen + 1;
+            }
+            idx++;
         }
         return -1;
     }
 
-    private int[] buildPrefixTableError(String needle) {
-        int len = needle.length();
-        int[] prefix = new int[len];
-        for (int i = 0; i < len; i++) {
-            int count = 0;
-            int lt = 0, rt = i;
-            while (lt <= rt) {
-                if (needle.charAt(lt) != needle.charAt(rt)) {
-                    break;
-                }
-                count++;
-                lt++;
-                rt--;
+    /**
+     * 构建KMP的部分匹配表， Partial Match Table
+     * 注意这里的部分匹配表和前后缀相等长度表是不同的，不应该弄混
+     * 关于KMP算法的理解：https://www.zhihu.com/question/21923021/answer/281346746
+     * 什么是部分匹配？
+     * a b a b a b
+     * a b a b _ _
+     * _ _ a b a b
+     * 这个字符串的部分匹配值是4，因为[0, 3]和[2, 5]是一样的
+     * 
+     * @param needle
+     * @return
+     */
+    private int[] preprocess(String needle) {
+        int idx = 1, matchIdx = 0; // idx 是当前处理的索引，matchIdx 是前缀长度
+        int[] table = new int[needle.length()];
+        while (idx < needle.length()) {
+            if (needle.charAt(idx) == needle.charAt(matchIdx)) {
+                // 当前字符匹配，增加匹配长度
+                matchIdx++;
+                table[idx] = matchIdx;
+                idx++;
+                continue;
             }
-            prefix[i] = count;
+            if (matchIdx > 0) {
+                // 不匹配时，回退到前一个匹配位置
+                matchIdx = table[matchIdx - 1];
+            } else {
+                // 如果无法回退，说明部分匹配值为0
+                table[idx] = 0;
+                idx++;
+            }
         }
-        return prefix;
+        return table;
     }
 
     /**
@@ -248,5 +264,245 @@ public class Interview150TwoEx {
             lt++;
         }
         return -1;
+    }
+
+    /**
+     * 盛最多水的容器
+     */
+    public int maxArea(int[] height) {
+        int lt = 0, rt = height.length - 1;
+        int res = 0;
+        while (lt < rt) {
+            int h = Math.min(height[lt], height[rt]);
+            int w = rt - lt;
+            res = Math.max(w * h, res);
+            if (height[lt] < height[rt]) {
+                lt++;
+            } else {
+                rt--;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 三数之和
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        for (int first = 0; first < len - 2; first++) {
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            int n1 = nums[first];
+            int second = first + 1, third = len - 1;
+            while (second < third) {
+                int n2 = nums[second], n3 = nums[third];
+                int sum = n1 + n2 + n3;
+                if (sum == 0) {
+                    res.add(List.of(n1, n2, n3));
+                    third--;
+                    while (third > second && nums[third] == nums[third + 1]) {
+                        third--;
+                    }
+                    second++;
+                    while (second < third && nums[second] == nums[second - 1]) {
+                        second++;
+                    }
+                } else if (sum < 0) {
+                    second++;
+                } else {
+                    third--;
+                }
+            }
+        }
+        return res;
+    }
+
+    public List<List<Integer>> threeSumTimeExceed(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        for (int first = 0; first < len - 2; first++) {
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            int n1 = nums[first];
+            for (int second = first + 1; second < len - 1; second++) {
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                int n2 = nums[second];
+                for (int third = second + 1; third < len; third++) {
+                    if (third > second + 1 && nums[third] == nums[third - 1]) {
+                        continue;
+                    }
+                    int n3 = nums[third];
+                    if (n1 + n2 + n3 == 0) {
+                        res.add(List.of(n1, n2, n3));
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 长度最小的子数组
+     */
+    public int minSubArrayLen(int target, int[] nums) {
+        int lt = 0, rt = 0;
+        int sum = 0;
+        int res = nums.length;
+        boolean hasFound = false;
+        while (rt < nums.length) {
+            sum += nums[rt];
+            while (sum >= target) {
+                hasFound |= true;
+                res = Math.min(res, rt - lt + 1);
+                sum -= nums[lt];
+                lt++;
+            }
+            rt++;
+        }
+        return hasFound ? res : 0;
+    }
+
+    /**
+     * 无重复字符的最长子串
+     */
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int lt = 0, rt = 0;
+        int len = s.length();
+        int maxLen = 0;
+        while (rt < len) {
+            char rc = s.charAt(rt);
+            while (set.contains(rc)) {
+                char lc = s.charAt(lt);
+                set.remove(lc);
+                lt++;
+            }
+            set.add(rc);
+            if (maxLen < (rt - lt + 1)) {
+                maxLen = rt - lt + 1;
+            }
+            rt++;
+        }
+        return maxLen;
+    }
+
+    /**
+     * 最小覆盖子串
+     */
+    public String minWindow(String s, String t) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> have = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        int valid = 0;
+        int lt = 0, rt = 0;
+        int sLen = s.length();
+        int start = -1, minLen = Integer.MAX_VALUE;
+        while (rt < sLen) {
+            char rc = s.charAt(rt);
+            if (need.containsKey(rc)) {
+                have.put(rc, have.getOrDefault(rc, 0) + 1);
+                if (have.get(rc).equals(need.get(rc))) {
+                    valid++;
+                }
+            }
+            while (valid >= need.size()) {
+                if (rt - lt + 1 <= minLen) {
+                    minLen = rt - lt + 1;
+                    start = lt;
+                }
+                char lc = s.charAt(lt);
+                if (need.containsKey(lc)) {
+                    if (have.get(lc).equals(need.get(lc))) {
+                        valid--;
+                    }
+                    have.put(lc, have.getOrDefault(lc, 0) - 1);
+                }
+                lt++;
+            }
+            rt++;
+        }
+        return start == -1 ? "" : s.substring(start, start + minLen);
+    }
+
+    /**
+     * 螺旋矩阵
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int row = matrix.length, col = matrix[0].length;
+        List<Integer> res = new ArrayList<>();
+        int rowIdx = 0, colIdx = 0;
+        int upBound = 0, downBound = row - 1;
+        int leftBound = 0, rightBound = col - 1;
+        int count = 0;
+        int total = row * col;
+        while (count < total) {
+            // right
+            for (int i = leftBound; count < total && i <= rightBound; i++) {
+                res.add(matrix[rowIdx][i]);
+                count++;
+                colIdx = i;
+            }
+            upBound++;
+            // down
+            for (int i = upBound; count < total && i <= downBound; i++) {
+                res.add(matrix[i][colIdx]);
+                count++;
+                rowIdx = i;
+            }
+            rightBound--;
+            // left
+            for (int i = rightBound; count < total && i >= leftBound; i--) {
+                res.add(matrix[rowIdx][i]);
+                count++;
+                colIdx = i;
+            }
+            downBound--;
+            // up
+            for (int i = downBound; count < total && i >= upBound; i--) {
+                res.add(matrix[i][colIdx]);
+                count++;
+                rowIdx = i;
+            }
+            leftBound++;
+        }
+        return res;
+    }
+
+    /**
+     * 旋转图像
+     */
+    public void rotate(int[][] nums) {
+        int row = nums.length, col = nums[0].length;
+        for (int i = 0; 2 * i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                int t = nums[i][j];
+                nums[i][j] = nums[row - 1 - i][j];
+                nums[row - 1 - i][j] = t;
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j <= i; j++) {
+                int t = nums[i][j];
+                nums[i][j] = nums[j][i];
+                nums[j][i] = t;
+            }
+        }
+    }
+
+    /**
+     * 矩阵置零
+     */
+    public void setZeroes(int[][] matrix) {
+
     }
 }
