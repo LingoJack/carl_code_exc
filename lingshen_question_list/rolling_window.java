df21ac39734420e81fbc59229585899ea8c1343f
lingojack <3065225677@qq.com>
2025-04-28 14:01:25 +0800

    1

--- lingshen_question_list/rolling_window.java
+++ lingshen_question_list/rolling_window.java
@@ -4,30 +4,9 @@ public class rolling_window {
 
     /**
      * 和相同的二元子数组
+     * TODO
      */
     public int numSubarraysWithSum(int[] nums, int goal) {
-        // 1 0 1 0 1
-        // 1 1 2 2 3
-        // l
-        // r
-        int len = nums.length;
-        int[] prefix = new int[len];
-        prefix[0] = nums[0];
-        for (int i = 1; i < len; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
-        }
-        int rt = len - 1;
-        int count = 0;
-        while (rt >= 0) {
-            int lt = 0;
-            while (lt <= rt && prefix[rt] - prefix[lt] >= goal) {
-                if (prefix[rt] - prefix[lt] == goal) {
-                    count++;
-                }
-                lt++;
-            }
-            rt--;
-        }
-        return count;
+        
     }
 }
