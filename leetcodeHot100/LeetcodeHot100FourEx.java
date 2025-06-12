ff5d981f356227b81b3705abedba681de29d7a1c
lingojack <3065225677@qq.com>
2025-06-13 00:19:26 +0800

    1

--- leetcodeHot100/LeetcodeHot100FourEx.java
+++ leetcodeHot100/LeetcodeHot100FourEx.java
@@ -2618,15 +2618,16 @@ public class LeetcodeHot100FourEx {
     public int minDistance(String word1, String word2) {
         int l1 = word1.length(), l2 = word2.length();
         int[][] dp = new int[l1 + 1][l2 + 1];
-        for(int i = 0; i < l2 + 1; i++) {
+        for (int i = 0; i < l2 + 1; i++) {
             dp[0][i] = i;
         }
-        for(int i = 0; i < l1 + 1; i++) {
+        for (int i = 0; i < l1 + 1; i++) {
             dp[i][0] = i;
         }
-        for(int i = 1; i < l1 + 1; i++) {
-            for(int j = 1; j < l2 + 1; j++) {
-                dp[i][j] = (word1.charAt(i  -1) == word2.charAt(j - 1)) ? (dp[i - 1][j - 1]) : (Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1);
+        for (int i = 1; i < l1 + 1; i++) {
+            for (int j = 1; j < l2 + 1; j++) {
+                dp[i][j] = (word1.charAt(i - 1) == word2.charAt(j - 1)) ? (dp[i - 1][j - 1])
+                        : (Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1);
             }
         }
         return dp[l1][l2];
@@ -2636,6 +2637,17 @@ public class LeetcodeHot100FourEx {
      * 只出现一次的数字
      */
     public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+
+    /**
+     * 多数元素
+     */
+    public int majorityElement(int[] nums) {
        
     }
 }
