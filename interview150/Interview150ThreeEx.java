f04715dc0ff8be2bed4f67588e748b24138df63a
lingojack <3065225677@qq.com>
2025-06-19 23:04:34 +0800

    1

--- interview150/Interview150ThreeEx.java
+++ interview150/Interview150ThreeEx.java
@@ -186,7 +186,22 @@ public class Interview150ThreeEx {
      * 买卖股票的最佳时机II
      */
     public int maxProfitII(int[] prices) {
+        int len = prices.length;
+        int[][] dp = new int[len][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[len - 1][0];
+    }
 
+    /**
+     * 跳跃游戏
+     */
+    public boolean canJump(int[] nums) {
+                
     }
 
     /**
